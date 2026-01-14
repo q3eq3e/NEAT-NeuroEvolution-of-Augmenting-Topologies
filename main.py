861127060a7f637753a01e250b8737196e611531
Patryk Skręta <01187282@pw.edu.pl>
2026-01-14 16:39:42 +0100

    Mode export/import mechanism

--- main.py
+++ main.py
@@ -2,14 +2,42 @@ from src.modeling.neat import NEAT
 from src.environment import Environment
 from src.environment import store_episode_as_gif
 from src.config import ENV_NAME
+from src.logger import FitnessLogger
+import pickle
+import datetime
+import argparse
 
 
-#                                                   !!!!!!!!!! zmien plik
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="NEAT")
+
+    parser.add_argument(
+        "model_file",
+        nargs="?",
+        help="Path to .pkl file",
+    )
+
+    args = parser.parse_args()
+
     env = Environment(ENV_NAME)
-    neat = NEAT(*env.get_params_num())
-    neat.train(env.run_env)
-    best = neat.get_best()
+
+    if args.model_file:
+        with open(args.model_file, "rb") as f:
+            best = pickle.load(f)
+
+    else:
+
+        neat = NEAT(*env.get_params_num())
+        fit_log = FitnessLogger()
+        neat.train(env.run_env, callbacks=[fit_log])
+        fit_log.save_chart_data("fitness_chart.png")
+        best = neat.get_best()
+
+        with open(
+            f"{ENV_NAME}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.pkl", "wb"
+        ) as f:
+            pickle.dump(best, f)
+
     print(best)
     print(best.nn)
     print(best.fitness)
